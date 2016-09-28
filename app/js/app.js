607e0c03aae1a398265e84611c1a64c78b6cda17
Muriel Gonzalez <murielgonzalez@gmail.com>
2016-09-28 17:35:02 -0500

    init

--- app/js/app.js
+++ app/js/app.js
@@ -1,7 +1,13 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var ReactFire = require('reactfire');
+var Firebase = require('firebase');
+var rootUrl = 'https://todos-1ed32.firebaseio.com/';
 
 var Hello = React.createClass({
+  componentWillMount : function () {
+    this.bindAsObject(new Firebase(rootUrl + 'items/'), 'items')
+  },
   render: function() {
     return <h1>
       Hello World!
